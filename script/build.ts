dce8a75aa972706030f906e58d72fe1f621e7438
Henry Blanchette <henry@monkeyface.com>
2026-02-06 13:17:23 -0500

    more advanced example

--- script/build.ts
+++ script/build.ts
@@ -1,14 +1,23 @@
 import fs from "fs/promises";
 import path from "path"
 
+// -----------------------------------------------------------------------------
+
 const pages_dirpath = "page"
+const dist_dirpath = "dist"
+
+// -----------------------------------------------------------------------------
 
 const page_filepaths =
     (await fs.readdir(pages_dirpath))
         .map((filename) => path.join(pages_dirpath, filename))
 
+await fs.rm(dist_dirpath, { recursive: true, force: true });
+
 await Bun.build({
     entrypoints: page_filepaths,
-    outdir: "dist",
+    outdir: dist_dirpath,
     minify: true,
 })
+
+
--- src/Main.purs
+++ src/Main.purs
@@ -3,6 +3,7 @@ module Main where
 import Prelude
 
 import Control.Monad.State (modify_)
+import Data.Foldable (fold)
 import Data.Maybe (Maybe(..), fromMaybe')
 import Effect (Effect)
 import Effect.Aff.Class (class MonadAff)
@@ -27,39 +28,85 @@ appComponent :: forall query input output m. MonadAff m => H.Component query inp
 appComponent = H.mkComponent { initialState, eval, render }
   where
   initialState _ =
-    { initTerm:
-        Input "name" \name -> Input "age" \age -> Input "message" \message ->
-          Output ("Hello, " <> name <> ", age " <> age <> ". Here's your message: " <> show message)
+    { term:
+        makeInputTerm "name" \name -> makeInputTerm "age" \age -> makeInputTerm "message" \message ->
+          makeOutputTerm ("Hello, " <> name <> ", age " <> age <> ". Here's your message: " <> show message)
     }
 
   eval = H.mkEval H.defaultEval
 
   render state =
     HH.div [ HP.classes [ HH.ClassName "app" ] ]
-      [ HH.div [] [ HH.text $ "initTerm = " <> show state.initTerm ]
-      , HH.slot_ (Proxy @"term") 0 termComponent { initTerm: state.initTerm }
+      [ HH.div [ HP.classes [ HH.ClassName "row" ] ] $ fold
+          [ [ HH.span_ [ HH.text "term =" ] ]
+          , displayTerm state.term
+          ]
+      , HH.slot_ (Proxy @"term") 0 termComponent { term: state.term }
       ]
 
 --------------------------------------------------------------------------------
 
 data Term
-  = Output String
-  | Input String (String -> Term)
+  = OutputTerm OutputTerm
+  | InputTerm InputTerm
+
+type OutputTerm =
+  { content :: String
+  }
+
+type InputTerm =
+  { label :: String
+  , k :: String -> Term
+  }
+
+makeOutputTerm :: String -> Term
+makeOutputTerm content = OutputTerm { content }
+
+makeInputTerm :: String -> (String -> Term) -> Term
+makeInputTerm label k = InputTerm { label, k }
 
 instance Show Term where
-  show (Output output) = "(Output " <> show output <> ")"
-  show (Input label k) = "(Input " <> show label <> " " <> show (k ("{{" <> label <> "}}")) <> ")"
+  show (OutputTerm { content }) = "(Output " <> show content <> ")"
+  show (InputTerm { label, k }) = "(Input " <> show label <> " " <> show (k ("{{" <> label <> "}}")) <> ")"
+
+displayTerm :: forall w i. Term -> Array (HH.HTML w i)
+displayTerm (OutputTerm { content }) =
+  [ HH.span [ HP.classes [ HH.ClassName "punctuation" ] ] [ HH.text "(" ]
+  , HH.span [ HP.classes [ HH.ClassName "constructor" ] ] [ HH.text "Output" ]
+  , HH.span [ HP.classes [ HH.ClassName "literal" ] ] [ HH.text content ]
+  , HH.span [ HP.classes [ HH.ClassName "punctuation" ] ] [ HH.text ")" ]
+  ]
+displayTerm (InputTerm { label, k }) = fold
+  [ [ HH.span [ HP.classes [ HH.ClassName "punctuation" ] ] [ HH.text "(" ]
+    , HH.span [ HP.classes [ HH.ClassName "constructor" ] ] [ HH.text "Input" ]
+    , HH.span [ HP.classes [ HH.ClassName "literal" ] ] [ HH.text label ]
+    ]
+  , displayTerm (k ("{{" <> label <> "}}"))
+  , [ HH.span [ HP.classes [ HH.ClassName "punctuation" ] ] [ HH.text ")" ] ]
+  ]
+
+data TermState
+  = OutputTermState
+      { term :: OutputTerm
+      }
+  | InputTermState
+      { term :: InputTerm
+      , result :: Maybe { content :: String, term :: Term }
+      }
+
+initTermState :: Term -> TermState
+initTermState (OutputTerm term) = OutputTermState { term }
+initTermState (InputTerm term) = InputTermState { term, result: Nothing }
 
 data TermAction
-  = SubmitInput (String -> Term)
+  = SubmitInput (String -> TermState)
   | Pass
 
-termComponent :: forall query input output m. MonadAff m => H.Component query (Record (initTerm :: Term | input)) output m
+termComponent :: forall query input output m. MonadAff m => H.Component query (Record (term :: Term | input)) output m
 termComponent = H.mkComponent { initialState, eval, render }
   where
   initialState input =
-    { oldTerm: input.initTerm
-    , mb_newTerm: Nothing @Term
+    { termState: initTermState input.term
     }
 
   eval = H.mkEval H.defaultEval
@@ -71,7 +118,7 @@ termComponent = H.mkComponent { initialState, eval, render }
               >>= (HTMLInputElement.fromElement >>> pure)
               >>= (fromMaybe' (\_ -> unsafeCrashWith "Impossible: KeyboardEvent target is not an HTMLInputElement.") >>> pure)
               >>= (HTMLInputElement.value >>> liftEffect)
-          modify_ _ { mb_newTerm = Just (k input) }
+          modify_ _ { termState = k input }
         Pass -> pure unit
     }
 
@@ -79,28 +126,46 @@ termComponent = H.mkComponent { initialState, eval, render }
     { input: H.RefLabel "input"
     }
 
-  render state = case state.oldTerm of
-    Output output ->
+  render state = case state.termState of
+    OutputTermState termState ->
       HH.div [ HP.classes [ HH.ClassName "term", HH.ClassName "Output" ] ]
-        [ HH.text output ]
-    Input label k ->
-      HH.div [ HP.classes [ HH.ClassName "term", HH.ClassName "Input" ] ]
-        [ HH.text label
-        , HH.input
-            [ HP.classes [ HH.ClassName "input" ]
-            , HP.ref refLabel.input
-            , HE.onKeyDown
-                ( \e ->
-                    if KeyboardEvent.key e == "Enter" then
-                      SubmitInput k
-                    else
-                      Pass
-                )
+        [ HH.div [ HP.classes [ HH.ClassName "row" ] ]
+            [ HH.span [ HP.classes [ HH.ClassName "item" ] ]
+                [ HH.text termState.term.content ]
             ]
-        , HH.button
-            [ HP.classes [ HH.ClassName "submit" ]
-            , HE.onClick (const (SubmitInput k))
-            ]
-            [ HH.text "Submit" ]
         ]
+    InputTermState termState ->
+      HH.div [ HP.classes [ HH.ClassName "term", HH.ClassName "Input" ] ]
+        case termState.result of
+          Nothing ->
+            [ HH.div [ HP.classes [ HH.ClassName "row" ] ]
+                [ HH.span [ HP.classes [ HH.ClassName "item" ] ]
+                    [ HH.text termState.term.label ]
+                , HH.input
+                    [ HP.classes [ HH.ClassName "input" ]
+                    , HP.ref refLabel.input
+                    , HE.onKeyDown
+                        ( \e ->
+                            if KeyboardEvent.key e == "Enter" then
+                              SubmitInput \s -> InputTermState termState { result = Just { term: termState.term.k s, content: s } }
+                            else
+                              Pass
+                        )
+                    ]
+                , HH.button
+                    [ HP.classes [ HH.ClassName "submit" ]
+                    , HE.onClick (const (SubmitInput \s -> InputTermState termState { result = Just { term: termState.term.k s, content: s } }))
+                    ]
+                    [ HH.text "Submit" ]
+                ]
+            ]
+          Just result ->
+            [ HH.div [ HP.classes [ HH.ClassName "row" ] ]
+                [ HH.span [ HP.classes [ HH.ClassName "item" ] ]
+                    [ HH.text termState.term.label ]
+                , HH.span [ HP.classes [ HH.ClassName "item" ] ]
+                    [ HH.text result.content ]
+                ]
+            , HH.slot_ (Proxy @"term") 0 termComponent { term: result.term }
+            ]
 
